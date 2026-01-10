e994148b2f886ee6d74a72049e9e64b2a10d79c5
kirti2818 <kirti8527260810@gmail.com>
2026-01-10 14:24:13 +0530

    change backend url

--- app/api.js
+++ app/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-// const baseURL = 'http://localhost:8080/api';
-const baseURL = 'https://backend-3-yzs6.onrender.com/api';
+const baseURL = 'http://localhost:8080/api';
+// const baseURL = 'https://backend-3-yzs6.onrender.com/api';
+
+let token;
 
 const api = axios.create({
     baseURL,
-    // headers: {
-    //     'Content-Type': 'application/json'
-    // },
     withCredentials: true
 })
 export default api;
