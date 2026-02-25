2e48519d6b0bb011739ea47b26a6596e21d1d587
kirti2818 <kirti8527260810@gmail.com>
2026-02-25 16:10:17 +0530

    Socket

--- app/socket.js
+++ app/socket.js
@@ -1,12 +1,13 @@
 import { io } from "socket.io-client";
 const token = (typeof window !== 'undefined') ? localStorage.getItem('token') : null;
 const baseURL = 'https://backend-3-yzs6.onrender.com'
+// const baseURL = "http://localhost:8080";
 const socket = io(baseURL, {
   // allow polling fallback so connection can still work when websocket upgrade isn't proxied
   transports: ['polling', 'websocket'],
   auth: token ? { token } : undefined,
   reconnection: true,
   withCredentials: true,
-  autoConnect: false,
+  // autoConnect: false,
 });
 export default socket;
